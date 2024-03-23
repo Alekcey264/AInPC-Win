40fa2addf178195b217c8079ed1b2230bdfe3179
Alekcey264 <alekseverevkin@gmail.com>
2024-03-24 01:16:26 +0500

    clean test file

--- test.py
+++ test.py
@@ -1,21 +1,2 @@
-from global_import import *
-
-def get_gpu_info(name):
-    try:
-        conn = sqlite3.connect(db)
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM gpu_info WHERE gpu_name = ?', (name,))
-        return cursor.fetchone()
-    except sqlite3.Error as error:
-        pass
-    finally:
-        if not cursor.fetchone():
-            message_box = QMessageBox()
-            message_box.setWindowTitle('Ошибка')
-            message_box.setText(f'Возникла ошибка в ходе получения информации о видеокарте {name}, пожалуйста, попробуйте открыть вкладку повторно или перезапустите программу.')
-            message_box.setIcon(QMessageBox.Icon.Warning)
-            message_box.exec()
-        if conn:
-            conn.close()
-
-print(get_gpu_info("asd"))
+def krappa123():
+    print("cya noobs")
